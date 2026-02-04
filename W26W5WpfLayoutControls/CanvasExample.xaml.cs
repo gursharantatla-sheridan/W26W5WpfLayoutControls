27f7c281151131be5436720f6dc237ce212a0628
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-02-04 14:07:05 -0500

    tab control eg

---
+++ W26W5WpfLayoutControls/CanvasExample.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace W26W5WpfLayoutControls
+{
+    /// <summary>
+    /// Interaction logic for CanvasExample.xaml
+    /// </summary>
+    public partial class CanvasExample : Window
+    {
+        public CanvasExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ W26W5WpfLayoutControls/TabControlExample.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace W26W5WpfLayoutControls
+{
+    /// <summary>
+    /// Interaction logic for TabControlExample.xaml
+    /// </summary>
+    public partial class TabControlExample : Window
+    {
+        public TabControlExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
